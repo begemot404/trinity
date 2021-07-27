5b0062bbd5216a6ae267e2c00e570078d7669ab1
begemot404 <artemgk93@gmail.com>
2021-07-27 11:33:48 +0400

    update

--- post-email.php
+++ post-email.php
@@ -3,11 +3,14 @@
 if (!$_POST) exit('No direct script access allowed');
 
 if (!isset($_POST['f'])) exit('No direct script access allowed');
-if (!isset($_POST['f']['name'])) exit('No direct script access allowed');
 if (!isset($_POST['f']['email'])) exit('No direct script access allowed');
 
+$firstname = trim(strip_tags($_POST['f']['firstname']));
+$lastname = trim(strip_tags($_POST['f']['lastname']));
+$company = trim(strip_tags($_POST['f']['company']));
 $email = trim(strip_tags($_POST['f']['email']));
-$name = trim(strip_tags($_POST['f']['name']));
+$phone = trim(strip_tags($_POST['f']['phone']));
+$message = trim(strip_tags($_POST['f']['message']));
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 {
@@ -18,14 +21,14 @@ if (!$name)
 {
 	exit('Не указано имя! Обновите страницу (F5) и укажите своё имя');
 }
+ 
 
+$to = 'business@aitrinity.net'; // адрес получателя
 
-$to = 'artemgajcuk@gmail.com'; // адрес получателя
-
-$subject = 'Тема письма'; // тема письма
+$subject = 'Contact Us | AI TRINITY'; // тема письма
 
 // формируем тело сообщения
-$message = 'Имя: ' . $name . "\r\n" . 'Email: ' . $email; 
+$message = 'First name: ' . $firstname . "\r\n" . ' Last name: ' . $lastname . "\r\n" . ' Company: ' . $company . "\r\n" . ' Email: ' . $email . "\r\n" . ' Phone: ' . $phone . "\r\n" . ' Message: ' . $message . "\r\n" . ' '; 
 
 // формируем headers для письма
 $headers = 'From: '. $email . "\r\n"; // от кого
